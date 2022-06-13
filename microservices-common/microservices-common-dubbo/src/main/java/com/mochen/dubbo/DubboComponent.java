f7ea6c190ad81c65d36e39374c9c0eb38fb8f148
yao_guangju@163.com <ygj125159>
2022-06-13 22:06:54 +0800

    整合dubbo

---
+++ microservices-common/microservices-common-dubbo/src/main/java/com/mochen/dubbo/DubboComponent.java
@@ -0,0 +1,10 @@
+package com.mochen.dubbo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan(basePackages = {"com.mochen.dubbo"})
+public class DubboComponent {
+
+}
